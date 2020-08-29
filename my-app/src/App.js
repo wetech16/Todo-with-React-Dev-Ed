172e1f3892953708c76e8847905d6497b5ad7b5f
Stephen Guo <wetech16@gmail.com>
2020-08-28 23:02:21 -0700

    -added state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,12 +5,13 @@ import TodoList from './components/TodoList';
 
 function App() {
   const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
         <header>
           <h1> Ed's Todo List </h1>
         </header>
-        <Form setInputText={setInputText}/>
+        <Form inputText={inputText} setInputText={setInputText} todos={todos} setTodos={setTodos} />
         <TodoList />
     </div>
   );
--- my-app/src/components/Form.js
+++ my-app/src/components/Form.js
@@ -1,14 +1,21 @@
 import React from "react";
 
-function Form({setInputText}) {
+function Form({inputText, setInputText, todos, setTodos}) {
   const inputTextHandler = (e) => {
     setInputText(e.target.value);
+  };
+  const submitTodoHandler = (e) => {
+      e.preventDefault();
+      setTodos([
+          ...todos, 
+          {text: inputText, completed: false, id: Math.random()*1000 }
+      ])
   }
   return (
     <div>
       <form>
         <input onChange={inputTextHandler} type="text" className="todo-input" />
-        <button className="todo-button" type="submit">
+        <button onClick={submitTodoHandler} className="todo-button" type="submit">
           <i className="fas fa-plus-square"></i>
         </button>
         <div className="select">
