ef53e37072b57577b79162101713b0729cdb65d2
Stephen Guo <wetech16@gmail.com>
2020-08-28 23:14:29 -0700

    -added components

--- my-app/src/components/TodoList.js
+++ my-app/src/components/TodoList.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Todo from "./Todo";
 
 function TodoList() {
   return (
     <div>
       <div className="todo-container">
-        <ul className="todo-list"></ul>
+        <ul className="todo-list">
+            <Todo />
+        </ul>
       </div>
     </div>
   );
