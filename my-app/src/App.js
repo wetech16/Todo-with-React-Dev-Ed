8e578b9692ed010365fcd28a2ab84b554f391267
Stephen Guo <wetech16@gmail.com>
2020-08-28 22:47:37 -0700

    -add components

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="App">
+        <header>
+          <h1> Ed's Todo List </h1>
+        </header>
+        <Form />
+        <TodoList />
+    </div>
+  );
+}
+
+export default App;
